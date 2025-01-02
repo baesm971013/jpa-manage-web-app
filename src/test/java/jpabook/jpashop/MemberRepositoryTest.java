a00c8922bb9fc6241a0aa2ca563bbc76d7cee285
baesm971013 <bobbyb537@naver.com>
2025-01-02 21:07:07 +0900

    build : add logging lib

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,13 +17,15 @@ class MemberRepositoryTest {
     @Test
     @DisplayName("저장 테스트")
     @Transactional
+    //@Rollback(false)
     public void test1() throws Exception{
         Member member = new Member();
-        member.setUserName("sumin");
+        member.setUserName("jack");
         Long savedMemberId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedMemberId);
-
+        System.out.println(findMember.getUserName());
         Assertions.assertThat(member.getUserName()).isEqualTo(findMember.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
 
     }
